9cc17bd0fd0553e19e1a4305ff53662ef9a4cc27
ZACKSHANE <zackyalhafizh1903@gmail.com>
2025-11-15 17:06:00 +0700

    Deploy build

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"; // <- tambahkan ini
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/pwd/", // <-- WAJIB untuk GitHub Pages
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
